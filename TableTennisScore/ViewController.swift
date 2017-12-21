2e89e74ed03c514b0c36cea39ce00852bbd4a745
Cecil Ma <machinemxy@me.com>
2017-12-21 21:47:04 +0900

    Initial Commit

---
+++ TableTennisScore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableTennisScore
+//
+//  Created by 马学渊 on 2017/12/21.
+//  Copyright © 2017年 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
