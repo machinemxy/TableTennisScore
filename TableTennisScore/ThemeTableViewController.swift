296521b854dd086a86092efc4e77c81d7708405e
Ma Xueyuan <machinemxy@me.com>
2018-10-15 22:30:54 +0900

    support iOS 12 add a link to pro table tennis scoreboard

--- TableTennisScore/ThemeTableViewController.swift
+++ TableTennisScore/ThemeTableViewController.swift
@@ -70,50 +70,10 @@ class ThemeTableViewController: UITableViewController {
 		let themeDetailViewController = segue.destination as! ThemeDetailViewController
 		themeDetailViewController.themeId = themeId
 	}
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func linkClicked(_ sender: Any) {
+        if let url = URL(string: "https://itunes.apple.com/us/app/pro-table-tennis-scoreboard/id1438386246?mt=8") {
+            UIApplication.shared.open(url, options: [:])
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
